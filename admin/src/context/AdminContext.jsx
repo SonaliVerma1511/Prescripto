e593f036ca385dcbaa9e7d159b7937a73814db55
Sonali Verma <sonaliverma2966920@gmail.com>
2025-08-09 00:41:37 +0530

    Admin,Frontend changes

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -11,7 +11,7 @@ const AdminContextProvider = (props) => {
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-  const getAlDoctors = async () => {
+  const getAllDoctors = async () => {
     try {
       const { data } = await axios.post(
         backendUrl + "/api/admin/all-doctors",
@@ -20,7 +20,7 @@ const AdminContextProvider = (props) => {
       );
       if (data.success) {
         setDoctors(data.doctors);
-        console.log(data.doctors)
+        console.log(data.doctors);
       } else {
         toast.error(data.message);
       }
@@ -28,12 +28,33 @@ const AdminContextProvider = (props) => {
       toast.error(error.message);
     }
   };
+
+  const changeAvailability = async (docId) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/change-availability",
+        { docId },
+        { headers: { aToken } }
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        getAllDoctors();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   const value = {
     aToken,
     setAToken,
     backendUrl,
     doctors,
     getAllDoctors,
+    changeAvailability,
   };
   return (
     <AdminContext.Provider value={value}>
