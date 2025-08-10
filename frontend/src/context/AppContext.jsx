7432d59ea78d433cfb24ac50fea52a0b8e1838fb
Sonali Verma <sonaliverma2966920@gmail.com>
2025-08-10 19:18:57 +0530

    new changes

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 import axios from "axios";
 import { toast } from "react-toastify";
@@ -9,29 +9,35 @@ const AppContextProvider = (props) => {
   const currencySymbol = "$";
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [doctors, setDoctors] = useState([]);
+  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):false);
 
-  const value = {
-    doctors,
-    currencySymbol,
-  };
+
 
   const getDoctorsData = async () => {
     try {
       const { data } = await axios.get(backendUrl + "/api/doctor/list");
       if (data.success) {
         setDoctors(data.doctors);
-      }else{
-        toast.error(data.message)
+      } else {
+        toast.error(data.message);
       }
     } catch (error) {
       console.log(error);
-      toast.error(error.message)
+      toast.error(error.message);
     }
+  };
+
+  const value = {
+    doctors,
+    currencySymbol,
+    token,
+    setToken,
+    backendUrl
   };
 
   useEffect(() => {
     getDoctorsData();
-  },[]);
+  }, []);
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
